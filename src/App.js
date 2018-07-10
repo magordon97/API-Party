c70352b13d707b013bf68be4d429ee38af3292a9
Matthew Gordon <magordon@anderson.edu>
2018-07-10 15:51:06 -0400

    Create form for Github component. No functionality

--- src/App.js
+++ src/App.js
@@ -1,6 +1,8 @@
 import React, { Component } from 'react';
 import logo from './logo.svg';
+import { Route, NavLink, Switch } from 'react-router-dom'
 import './App.css';
+import GitHub from './Github'
 
 class App extends Component {
   render() {
@@ -8,11 +10,22 @@ class App extends Component {
       <div className="App">
         <header className="App-header">
           <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
+          <h3>There' no party like an</h3>
+          <h1 className="App-title">API Party</h1>
         </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
+
+        <ul className="navLinks">
+          <li>
+            <NavLink to="/github">GitHub API</NavLink>
+          </li>
+        </ul>
+
+        <Switch>
+          <Route path="/github" component={GitHub} />
+          <Route render={()=>(
+            <p>To get started, click one of the links above. </p>
+          )} />
+        </Switch>
       </div>
     );
   }
---
+++ src/Github.js
@@ -0,0 +1,31 @@
+import React, { Component } from 'react'
+
+class Github extends Component {
+  render() {
+    return (
+      <div className="Github">
+        <img
+          src="http://www.aha.io/assets/github.7433692cabbfa132f34adb034e7909fa.png"
+          alt="GitHub"
+          className="logo"
+        /> 
+        <form>
+          <div>
+            <input
+              required
+              autoFocus
+              type="text"
+            />
+          </div>
+          <div>
+            <button type="submit">
+              Look up GitHub user
+            </button>
+          </div>
+        </form>
+      </div>
+    )
+  }
+}
+
+export default Github
